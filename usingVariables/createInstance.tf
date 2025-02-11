a8b8d8082cd913ebb8fd4d2450ebddbd88a93ac5
PradipDhanraj <dhanraj.pd@pg.com>
2025-02-11 16:22:46 +0530

    fix: modified structure

---
+++ usingVariables/createInstance.tf
@@ -0,0 +1,10 @@
+
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 1
+  ami           = "ami-0bff25b43a4479334"
+  instance_type = "t4g.micro"
+
+  tags = {
+    Name = "demoinstnce-${count.index}"
+  }
+}
